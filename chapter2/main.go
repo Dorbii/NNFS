062d4ee769e07e39bdc7bacd4689014d87298455
steve.doris <stevemdoris@gmail.com>
2023-11-09 22:34:05 -0600

    Added additional input and weight

--- chapter2/main.go
+++ chapter2/main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	inputs := []float32{1, 2, 3}
-	weights := []float32{0.2, 0.8, -0.5}
+	inputs := []float32{1.0, 2.0, 3.0, 2.5}
+	weights := []float32{0.2, 0.8, -0.5, 1.0}
 	var bias float32 = 2
 
 	output := (inputs[0]*weights[0] +
 		inputs[1]*weights[1] +
-		inputs[2]*weights[2] + bias)
+		inputs[2]*weights[2] +
+		inputs[3]*weights[3] + bias)
 	fmt.Println(output)
 }
