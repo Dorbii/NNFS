2d3e1d9da829e42051b46896ddbaf05b0d5c4683
steve.doris <stevemdoris@gmail.com>
2023-11-18 21:37:55 -0600

    added goZip pkg

--- internal/chapter2/chapter2.go
+++ internal/chapter2/chapter2.go
@@ -1,14 +1,17 @@
 package chapter2
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func Chapter2() string {
 	inputs := []float32{1.0, 2.0, 3.0, 2.5}
-	//weights := [][]float32{{0.2, 0.8, -0.5, 1.0}, {0.5, -0.91, 0.26, -0.5}, {-0.26, -0.27, 0.17, 0.87}}
-	//biases := []float32{2.0, 3.0, 0.5}
+	weights := [][]float32{{0.2, 0.8, -0.5, 1.0}, {0.5, -0.91, 0.26, -0.5}, {-0.26, -0.27, 0.17, 0.87}}
+	biases := []float32{2.0, 3.0, 0.5}
 
-	//layer_outputs := []float32{}
+	layer_outputs := []float32{}
 
+	for nweights, nbias 
 	// outputs := []float32{
 	// 	inputs[0]*weights1[0] +
 	// 		inputs[1]*weights1[1] +
---
+++ pkg/goZip/goZip.go
@@ -0,0 +1,22 @@
+package goZip
+
+import (
+	"fmt"
+)
+
+type Pair[A, B any] struct {
+	First  A
+	Second B
+}
+
+func GoZip[A, B any](a []A, b []B) ([]Pair[A, B], error) {
+	if len(a) != len(b) {
+		return nil, fmt.Errorf("Length of slices are not equal")
+	}
+	pairs := make([]Pair[A, B], len(a))
+	for i := range a {
+		pairs[i] = Pair[A, B]{a[i], b[i]}
+	}
+	return pairs, nil
+
+}
